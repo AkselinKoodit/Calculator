f9f010ffdeaf1f8f8346633e303daa2b622125ca
Akseli <s2100140@edu.bc.fi>
2021-02-25 14:07:18 +0200

    added deleting feature and cleaned a little

--- script.js
+++ script.js
@@ -9,43 +9,53 @@ let result = 0;
  */
 
 function performCalculation() {
-  const firstNumberAsNumber = Number(firstNumber);
-  const secondNumberAsNumber = Number(secondNumber);
+  const previousNumber = Number(firstNumber);
+  const currentNumber = Number(secondNumber);
   if (operation === "+") {
-    result = secondNumberAsNumber + firstNumberAsNumber;
+    result = currentNumber + previousNumber;
   } else if (operation === "-") {
-    result = secondNumberAsNumber - firstNumberAsNumber;
+    result = currentNumber - previousNumber;
   } else if (operation === "*") {
-    result = secondNumberAsNumber * firstNumberAsNumber;
+    result = currentNumber * previousNumber;
   } else if (operation === "/") {
-    result = secondNumberAsNumber / firstNumberAsNumber;
+    result = currentNumber / previousNumber;
   } else {
     result = "Error: 80085";
   }
   document.getElementById("input").innerText = `${result}`;
   firstNumber = "";
   secondNumber = "";
-  operator = "";
+  operation = "";
 }
 
 document.getElementById("equals").addEventListener("click", performCalculation);
 
+//Selecting number:
 document.querySelectorAll(".numbers > div").forEach((button) => {
   updateDisplay();
   button.addEventListener("click", function () {
-    console.log(firstNumber);
+    //let's make sure there's no "leftover" operation
+    if (secondNumber === "" && operation != "") {
+      operation = "";
+      updateDisplay();
+    }
+    //then make sure you can only add on ".":
     if (this.innerText === "." && firstNumber.indexOf(".") > -1) {
       return;
     } else {
+      //then the actual number:
       firstNumber += this.innerText;
     }
     updateDisplay();
   });
 });
 
+/**
+ * next updates the operation with user choice.
+ *@return {void}
+ */
 document.querySelectorAll(".operators > div").forEach((button) => {
   button.addEventListener("click", function () {
-    console.log(this.innerText);
     secondNumber = firstNumber;
     firstNumber = "";
     switch (this.innerText) {
@@ -70,16 +80,6 @@ document.querySelectorAll(".operators > div").forEach((button) => {
 });
 
 /**
- * updates the operation with user choice.
- *@return {void}
- */
-function updateSelectedOperation() {
-  console.log(this.innerTextt);
-}
-
-function updateSelectedNumber() {}
-
-/**
  * updates what's shown on calculator display. Note that operator-button changes firstnumber to second and that's why second needs to be displayed before operator and "new first number"
  */
 function updateDisplay() {
@@ -95,4 +95,23 @@ function reset() {
   secondNumber = "";
   operation = "";
   updateDisplay();
+}
+
+//Deleting input:
+document.getElementById("remove").addEventListener("click", deleteLast);
+
+function deleteLast() {
+  if (firstNumber != "") {
+    firstNumber = firstNumber.toString().slice(0, -1);
+    updateDisplay();
+  } else if (firstNumber == "" && operation != "") {
+    operation = "";
+    updateDisplay();
+  } else if (firstNumber == "" && operation == "") {
+    console.log(secondNumber);
+    secondNumber = secondNumber.toString().slice(0, -1);
+    updateDisplay();
+  } else {
+    console.log("Something went wrong...");
+  }
 }
